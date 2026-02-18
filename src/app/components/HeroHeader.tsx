afd0c214c0a2db39cb47891bb5a01efcbbcbf851
Troy Morgan <jemmarmorgan@gmail.com>
2026-02-19 07:10:08 +1000

    Slow down WIN $500 ticker another 10%

--- src/app/components/HeroHeader.tsx
+++ src/app/components/HeroHeader.tsx
@@ -63,7 +63,7 @@ export function HeroHeader({ drawDate }: HeroHeaderProps) {
           }
           .ticker-inner {
             display: inline-flex;
-            animation: ticker 60s linear infinite;
+            animation: ticker 66s linear infinite;
             white-space: nowrap;
           }
         `}</style>
